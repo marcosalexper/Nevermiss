29649891808403a4a8cc3bed0f766c561df95d65
marcosalexper <marcosalexper@hotmail.com>
2026-07-29 14:36:08 -0300

    create application base layout

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,25 @@
-console.log(
-    "Nevermiss application started"
-);
+async function loadComponent(path) {
+
+    const response = await fetch(path);
+
+    return await response.text();
+
+}
+
+async function start() {
+
+    const app = document.getElementById("app");
+
+    const layout = await loadComponent("./layouts/main.html");
+
+    app.innerHTML = layout;
+
+    document.getElementById("sidebar").innerHTML =
+        await loadComponent("./components/sidebar/sidebar.html");
+
+    document.getElementById("header").innerHTML =
+        await loadComponent("./components/header/header.html");
+
+}
+
+start();
